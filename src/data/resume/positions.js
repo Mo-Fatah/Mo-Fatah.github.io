09fbec4e1c577c4962b6674e7d642ea5a3e12567
mohamed <mohamedabdelfatah2027@gmail.com>
2022-11-12 20:11:34 +0200

    Update Resume link

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -8,7 +8,7 @@ const positions = [
       'Running WebAssembly outside the browser requires different capabilites like accessing the File System, I/O, Network access.',
       'All WASM engines have their own interpretation of the WASI standard.',
       'I was responsible for providing a testsuite that can be run against different WASM engines implementation of WASI.',
-      'I ran my testcases mainly against Wasmer and Wasmtime and I caught multiple unconsistent behaviors and I opened multiple issues which led to either fix their WASI implementation or imporve the WASI standard.',
+      'I ran my testcases mainly against Wasmer and Wasmtime and I caught multiple inconsistent behaviors and I opened multiple issues which led to either fix their WASI implementation or imporve the WASI standard.',
     ],
     technologies: [
       'Rust, ',
